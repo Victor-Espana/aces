d5a4b63dac0182c3befe378be24fb5fa53bdc4e5
victo <victorER98@gmail.com>
2022-11-10 16:57:16 +0100

    Big update

--- R/AAFS.R
+++ R/AAFS.R
@@ -1,4 +1,4 @@
-#' @title Additive Adaptive Frontier Splines
+#' @title Additive Adaptive Frontier Splines: 1 output
 #'
 #' @description This function estimates a production frontier satisfying some classical production theory axioms, such as monotonicity and concavity, which is based upon the adaptation of the machine learning technique known as Multivariate Adaptive Regression Splines (MARS) developed by \insertCite{friedman1991;textual}{aafs}.
 #'
@@ -22,17 +22,11 @@
 #' \item{\code{endspan = -1}} Computed as in \insertCite{friedman1991;textual}{aafs}.
 #' \item{\code{endspan = +m}}
 #' }
-#' @param knotsGrid For the Forward algorithm. Grid of knots to perform AAFS:
-#' \itemize{
-#' \item{\code{knotsGrid = -2}} A grid of equidistant knots is created from the scattering of the data.
-#' \item{\code{knotsGrid = -1}} The original approach \insertCite{friedman1991}{aafs} based on the observed data is used.
-#' \item{\code{knotsGrid = +p}} A grid of \code{p} percentiles is computed.
-#' }
-#' @param lambda For the Forward algorithm. The lasso penalty parameter. Default is \code{0}.
+#' @param knotsGrid For the Forward algorithm. If \code{knotsGrid == -1} (default), the original approach of \insertCite{friedman1991;textual}{aafs} based on the observed data is used. Alternatively, a \code{list} with the grid of knots to perform AAFS can be entered. In this case, each element of the \code{list} contains a vector with the knots of one variable (e.g., the second element of the list contains the knot values of the second variable).
 #' @param d For the Backward algorithm. Generalized Cross Validation (GCV) penalty per knot. Default is \code{2}.
 #' @param w1 For the cubic smoothing procedure \insertCite{friedman1991}{aafs}. The distance between the central knot and a side knot is at most \code{w} times the distance between that central knot and the other side knot for each truncated cubic basis function. It must be set between 1 and 2. If a \code{vector} is entered, the \code{w} value that most reduce the lack-of-fit is selected.
 #' @param w2 For the quintic smoothing procedure \insertCite{chen1999}{aafs}. The distance between the central knot and a side knot is at most \code{w} times the distance between that central knot and the other side knot for each truncated quintic basis function. It must be set between 8/7 and 1.5. If a \code{vector} is entered, the \code{w} value that most reduce the lack-of-fit is selected.
-#' @param linpreds \code{logical}. If \code{TRUE}, predictors can enter linearly.
+#' @param weights Weights for outputs.
 #' @param na.rm \code{logical}. If \code{TRUE}, \code{NA} rows are omitted.
 #'
 #' @references
@@ -48,15 +42,15 @@
 #'
 #' @export
 AAFS <- function(data, x, y, nterms, Kp = 1, err.red = 0.01,
-                 minspan = -2, endspan = -1, knotsGrid = -1,
-                 lambda = 0, d = 2, w1 = seq(1, 2, 0.25),
-                 w2 = seq(8/7, 1.5, 0.1), linpreds = FALSE,
+                 minspan = -2, endspan = -2, knotsGrid = -1,
+                 d = 2, w1 = seq(1, 2, length.out = 5),
+                 w2 = seq(8 / 7, 1.5, length.out = 5),
                  na.rm = TRUE) {
 
   # Hyperparameters errors
   if (Kp != 1) stop("Kp can only be 1.")
-  if (!all(w1 > 1 & w1 < 2)) stop("w1 must be between 1 and 2")
-  if (!all(w2 > 8 / 7 & w2 < 1.5)) stop("w2 must be between 8/7 and 1.5")
+  if (!all(w1 >= 1 & w1 <= 2)) stop("w1 must be between 1 and 2")
+  if (!all(w2 >= 8 / 7 & w2 <= 1.5)) stop("w2 must be between 8/7 and 1.5")
 
   # Data in data[x, y] format.
   data <- preProcess(data = data, x = x, y = y, na.rm = na.rm)
@@ -71,8 +65,7 @@ AAFS <- function(data, x, y, nterms, Kp = 1, err.red = 0.01,
   x <- 1:(ncol(data) - length(y))
   y <- (length(x) + 1):ncol(data)
 
-  # nterms
-  # Max value between hyperparameter selected by the user and C(M) < N during backward
+  # nterms: max value between nterms and C(M) < N during backward
   nterms <- min(nterms, floor((2 * N + d) / (2 + d)))
 
   # ================= #
@@ -105,37 +98,16 @@ AAFS <- function(data, x, y, nterms, Kp = 1, err.red = 0.01,
   # Set of basis functions and B Matrix
   ForwardModel <- list(BF = list(bf), B = matrix(rep(1, N)))
 
-  # Error of first basis function
+  # Error of the first basis function
   err <- bf[["R"]]
+  max.err.y <- which.max(err) # maximum error id
+  try.y <- rep(TRUE, length(y))
 
   # Minimum span (minspan) and end span (endspan)
   L.Le <- computeSpan(data, minspan, endspan, nX)
 
-  # Grid of knots
-  if (knotsGrid == - 2) {
-    kg <- - 2 # for AAFS object
-    knotsGrid <- vector("list", nX)
-
-    for(var in 1:nX) {
-      # Median distance
-      median.distance  <- median(diff(data[order(data[, var]), var]))
-      knotsGrid[[var]] <- seq(min(data[, var]), max(data[, var]), median.distance)
-    }
-
-  } else if (knotsGrid > 0) {
-    kg <- knotsGrid # for AAFS object
-    percentile <- knotsGrid
-    knotsGrid  <- vector("list", nX)
-
-    for(var in 1:nX) {
-      knotsGrid[[var]] <- quantile(data[, var],
-                                   probs = seq(0, 1, 1 / (percentile - 1)),
-                                   names = FALSE)
-    }
-
-  } else {
-    kg <- knotsGrid # for AAFS object
-    knotsGrid <- NULL
+  if (!is.list(knotsGrid)) {
+    knotsGrid <- lapply(1:nX, function(i) data[, i])
   }
 
   while(length(ForwardModel[["BF"]]) + 2 < nterms) {
@@ -145,21 +117,35 @@ AAFS <- function(data, x, y, nterms, Kp = 1, err.red = 0.01,
                             ForwardModel,
                             knots.list, Kp,
                             L.Le[[1]], L.Le[[2]],
-                            knotsGrid, lambda,
-                            linpreds,
-                            err.min = err)
+                            knotsGrid,
+                            err.min = err,
+                            err.idx = max.err.y)
 
-     ForwardModel[["B"]] <- B.BF.knots.err[[1]]
-    ForwardModel[["BF"]] <- B.BF.knots.err[[2]]
-              knots.list <- B.BF.knots.err[[3]]
-                 new.err <- B.BF.knots.err[[4]]
+    new.err <- B.BF.knots.err[[4]]
 
     # New minimum error
-    if (new.err < err * (1 - err.red)) {
-      err <- new.err
+    if (mean(unlist(new.err)) < mean(unlist(err)) * (1 - err.red)) {
 
-    } else {
-      break
+      # If the reduction of the error is enough...
+      # Update B
+      ForwardModel[["B"]]  <- B.BF.knots.err[[1]]
+      # Update basis functions
+      ForwardModel[["BF"]] <- B.BF.knots.err[[2]]
+      # Update the knot list
+      knots.list <- B.BF.knots.err[[3]]
+      # Update error
+      err <- new.err
+      max.err.y <- which.max(err) # maximum error id
+      try.y <- rep(TRUE, length(y))
+
+    } else if (any(try.y)){
+      try.y[max.err.y] <- FALSE
+
+      if (!any(try.y)) {
+        break
+      } else {
+        max.err.y <- c(1:length(y))[try.y][1]
+      }
     }
   }
 
@@ -167,8 +153,8 @@ AAFS <- function(data, x, y, nterms, Kp = 1, err.red = 0.01,
   # Forward AAFS
   # ==
 
-     xi <- unlist(sapply(ForwardModel[["BF"]], function(x) if(all(x[["xi"]] != -1)) x[["xi"]]))
-      t <- unlist(sapply(ForwardModel[["BF"]], function(x) if(all(x[["xi"]] != -1)) x[["t"]]))
+  xi <- unlist(sapply(ForwardModel[["BF"]], function(x) if(all(x[["xi"]] != -1)) x[["xi"]]))
+   t <- unlist(sapply(ForwardModel[["BF"]], function(x) if(all(x[["xi"]] != -1)) x[["t"]]))
   coefs <- ForwardModel[["BF"]][[length(ForwardModel[["BF"]])]][["coefs"]]
 
   knotsForward <- unique(cbind(xi, t))
@@ -183,25 +169,25 @@ AAFS <- function(data, x, y, nterms, Kp = 1, err.red = 0.01,
   # ================== #
   # BACKWARD ALGORITHM #
   # ================== #
-  SubModels.AAFS <- PruningAAFS(data, y, AAFS.Forward, d)
+  AAFS.Submodels <- PruningAAFS(data, y, AAFS.Forward, d)
 
   # Lack-of-fit at each model
-  GCVs <- sapply(SubModels.AAFS, function(x) x[["GCV"]])
+  GCVs <- sapply(AAFS.Submodels, function(x) x[["GCV"]])
 
   # Model with minimum error
-  BackwardModel <- SubModels.AAFS[[which.min(GCVs)]]
-  knotsBackward <- do.call(rbind.data.frame, BackwardModel[["t"]])
+  AAFS.Backward <- AAFS.Submodels[[which.min(GCVs)]]
+  knotsBackward <- do.call(rbind.data.frame, AAFS.Backward[["t"]])
 
   # ==
   # AAFS Model
   # ==
 
-  AAFS_Model <- list(
-    AAFS_SubModels = SubModels.AAFS,
-                 B = BackwardModel[["B"]],
+  AAFS <- list(
+    AAFS_SubModels = AAFS.Submodels,
+                 B = AAFS.Backward[["B"]],
              knots = knotsBackward,
-             coefs = BackwardModel[["coefs"]],
-               GCV = BackwardModel[["GCV"]]
+             coefs = AAFS.Backward[["coefs"]],
+               GCV = AAFS.Backward[["GCV"]]
     )
 
   # =================== #
@@ -213,22 +199,35 @@ AAFS <- function(data, x, y, nterms, Kp = 1, err.red = 0.01,
   # ==
 
   if (nrow(knotsBackward) == 0) {
-    Smooth_AAFS <- AAFS_Model
+    CSmooth.AAFS <- QSmooth.AAFS <- AAFS.Model
     warning("Smoothing not available because there are no knots. AAFS model is used instead.")
 
   } else {
-    CSmooth_AAFS <- CSAAFS(data, nX, knotsBackward, y, w1)
-    QSmooth_AAFS <- QSAAFS(data, nX, knotsBackward, y, w2)
+    # List for x.space
+    x.space <- vector("list", nX)
+
+    for (xi in 1:nX) {
+      # Knots for the xi variable
+      kt_xi <- sort(unique(knotsBackward[knotsBackward[, 1] == xi, 2]))
+
+      if (length(kt_xi) == 0) next
+
+      # Add the initial and the end observation. They cannot be used as knots.
+      x.space[[xi]] <- c(min(data[, xi]), kt_xi, max(data[, xi]))
+
+      # Calculate Midpoints
+      x.space[[xi]] <- sort(c(x.space[[xi]], x.space[[xi]][- length(x.space[[xi]])] + diff(x.space[[xi]]) / 2))
+    }
+
+    CSmooth.AAFS <- CSAAFS(data, nX, knotsBackward, x.space, y, w1)
+    QSmooth.AAFS <- QSAAFS(data, nX, knotsBackward, x.space, y, w2)
   }
 
   # AAFS object
-  AAFS <- AAFS_object(data, x, y,
-                      nterms, Kp, d,
-                      err.red, na.rm,
-                      AAFS_Forward,
-                      AAFS_Model,
-                      QSmooth_AAFS,
-                      CSmooth_AAFS)
+  AAFS <- AAFS_object(data, x, y, nterms, Kp, err.red,
+                      minspan, endspan, knotsGrid, d,
+                      na.rm, AAFS.Forward, AAFS,
+                      QSmooth.AAFS, CSmooth.AAFS)
 
   return(AAFS)
 }
@@ -250,10 +249,7 @@ AAFS <- function(data, x, y, nterms, Kp = 1, err.red = 0.01,
 #' \item{\code{endspan = -1}} Computed as in \insertCite{friedman1991;textual}{aafs}.
 #' \item{\code{endspan = +m}}
 #' }
-#'
 #' @param nX Number of inputs.
-#'
-#' @importFrom Rdpack reprompt
 #'
 #' @return \code{list} with the minimum span and the end span.
 computeSpan <- function(data, minspan, endspan, nX) {
@@ -311,7 +307,6 @@ computeSpan <- function(data, minspan, endspan, nX) {
   }
 
   return(list(L, Le))
-
 }
 
 #' @title Create an AAFS object
@@ -322,9 +317,12 @@ computeSpan <- function(data, minspan, endspan, nX) {
 #' @param x Column input indexes in \code{data}.
 #' @param y Column output indexes in \code{data}.
 #' @param nterms Maximum number of terms created by the forward algorithm (before pruning).
-#' @param Kp Maximum degree of interaction allowed. Default is \code{1}.
-#' @param d Generalized Cross Validation (GCV) penalty per knot. Default is \code{2}. If set to \code{-1}, \code{GCV = RSS / n}.
-#' @param err.red Minimun reduced error rate for the addition of two new basis functions. Default is \code{0.01}.
+#' @param Kp Maximum degree of interaction allowed. Only \code{1} is available.
+#' @param err.red Minimum reduced error rate for the addition of two new basis functions.
+#' @param minspan Minimum number of observations between two adjacent knots.
+#' @param endspan Minimum number of observations before the first and after the final knot.
+#' @param knotsGrid Grid of knots to perform AAFS.
+#' @param d Generalized Cross Validation (GCV) penalty per knot.
 #' @param na.rm \code{logical}. If \code{TRUE}, \code{NA} rows are omitted.
 #' @param Forward.AAFS \code{list} containing the Forward Additive Adaptive Frontier Splines.
 #' @param AAFS \code{list} containing the Additive Adaptive Frontier Splines model.
@@ -334,43 +332,31 @@ computeSpan <- function(data, minspan, endspan, nX) {
 #' @importFrom dplyr %>% filter select
 #'
 #' @return A \code{AAFS} object.
-AAFS_object <- function(data, x, y, nterms, Kp, d, err.red, na.rm,
-                        Forward.AAFS, AAFS, QSmooth.AAFS, CSmooth.AAFS) {
+AAFS_object <- function(data, x, y, nterms, Kp, err.red,
+                        minspan, endspan, knotsGrid, d,
+                        na.rm, Forward.AAFS, AAFS, QSmooth.AAFS,
+                        CSmooth.AAFS) {
 
-  AAFS_object <- list("data" = list(df = data,
+  AAFS.object <- list("data" = list(df = data,
                                     x  = x,
                                     y  = y,
                                     input_names  = colnames(data)[x],
                                     output_names = colnames(data)[y],
                                     row_names    = rownames(data)),
                       "control" = list(nterms = nterms,
-                                        Kp = Kp,
-                                        d  = d,
-                                        err.red = err.red,
-                                        na.rm = na.rm),
+                                       Kp = Kp,
+                                       err.red = err.red,
+                                       minspan = minspan,
+                                       endspan = endspan,
+                                       knotsGrid = knotsGrid,
+                                       d  = d,
+                                       na.rm = na.rm),
                       "Forward.AAFS" = Forward.AAFS,
                       "AAFS"         = AAFS,
-                      "QSmooth.AAFS" = QSmooth.AAFS,
-                      "CSmooth.AAFS" = CSmooth.AAFS)
+                      "CSAAFS"       = CSmooth.AAFS,
+                      "QSAAFS"       = QSmooth.AAFS)
 
-  class(AAFS_object) <- "AAFS"
+  class(AAFS.object) <- "AAFS"
 
-  # Data for Efficiency Models
-  knots <- unique(AAFS[["knots"]][, c("xi", "t")])
-  knots.list <- vector("list", length(x))
-
-  for (v in 1:length(x)) {
-    knots.list[[v]] <- knots %>% filter(xi == v) %>% select(t) %>% t()
-    knots.list[[v]] <- sort(c(knots.list[[v]], min(data[, v]), max(data[, v])))
-  }
-
-  aknots <- expand.grid(knots.list)
-  names(aknots) <- colnames(data)[x]
-
-  yknots <- predict(AAFS_object, aknots, x, 2)
-
-  AAFS_object[["Efficiency"]] <- list("aknots" = aknots,
-                                      "yknots" = yknots)
-
-  return(AAFS_object)
+  return(AAFS.object)
 }
